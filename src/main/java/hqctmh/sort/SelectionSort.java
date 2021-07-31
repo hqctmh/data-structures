738d8031c71dd55292f1a9fc8f2bcf4773ec18cd
mh <258479949@qq.com>
2021-07-31 22:54:35 +0800

    实现选择排序算法

---
+++ src/main/java/hqctmh/sort/SelectionSort.java
@@ -0,0 +1,45 @@
+package hqctmh.sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @Author mh
+ * @Description 选择排序
+ * @Date 2021/7/31
+ */
+public class  SelectionSort {
+
+    public static <T> void sort(T[] array, Comparator<T> comparator) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = -1;
+            for (int j = i + 1; j < array.length; j++) {
+                if (comparator.compare(array[i], array[j])>0) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex > 0) {
+                swap(array, i, minIndex);
+            }
+        }
+    }
+
+    private static <T> void swap(T[] array, int i, int j) {
+        T temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+    public static void main(String[] args) {
+
+        Integer[] array = new Integer[5];
+        for (int i = 5; i > 0; i--) {
+            array[i-1] = i;
+        }
+
+        sort(array, Comparator.comparingInt(i -> i));
+        System.out.println(Arrays.toString(array));
+    }
+}
